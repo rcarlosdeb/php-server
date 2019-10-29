266a3aacabde0893a4cbb364a1a0aa20ce129e93
rcarlosdeb <rcarlosdeb@gmail.com>
2019-10-29 02:02:28 -0600

    Agregando los enlaces para login y registro

--- public_html/index.php
+++ public_html/index.php
@@ -13,5 +13,23 @@
 <br><br> Lista de todos los usuarios actualmente en la base de datos, no se muestra la contraseña para evitar fallos de seguridad
 </p>
 
+<p>- <a href="http://35.232.215.93/apis/tipo-usuario.php">Tipos de usuario</a>
+<br><br> Lista de los tipos de usuario que manejara el sistema
+</p>
+
+<p>- <a href="http://35.232.215.93/apis/login.php">Verificar el usuario y contraseña</a>
+<br><br> Se debera usar la url anterior para hacer POST de un json que debe contener al menos las propiedades email y pass:
+<br>{email:'rcarlosdeb@gmail.com', pass:'contrasenia'}
+<br> El resultado para un usuario valido es un estatus 200 y todos los detalle del usuario que se ha buscado
+<br> Cuando el usuario sea incorrecto el resultado es un estatus 404 
+</p>
+
+<p>- <a href="http://35.232.215.93/apis/singup.php">Registro de usuario</a>
+<br><br> Se debera usar la url anterior para hacer POST de un json que debe contener al menos las propiedades: nombres, email, pass, id_tipo_usuario
+<br>{nombres:'Roberto Carlos', email:'rcarlosdeb@gmail.com', pass:'contrasenia', id_tipo_usuario:2}
+<br> El resultado para registro correcto es un estatus 200
+<br> El resultado para un registro incorrecto por falta de propiedades u otro error es estatus 404
+</p>
+
 </body>
 </html>
