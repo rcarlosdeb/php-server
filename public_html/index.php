852aeb5ca8be1312420119c2770abae871261cc4
rcarlosdeb <rcarlosdeb@gmail.com>
2019-10-29 01:41:45 -0600

    primer enlace para todos los usuarios

--- public_html/index.php
+++ public_html/index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gym</title>
+    <title>Asistencia</title>
 </head>
 <body>
 
 <h4>Aqui se iran listando los recursos disponibles para consumir desde la aplicacion de flutter</h4>
+<p>- <a href="http://35.232.215.93/apis/user.php">Todos los usuarios</a>
+</p>
 
 </body>
 </html>
