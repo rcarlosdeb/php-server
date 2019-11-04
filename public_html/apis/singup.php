cda03e656ab90617cb2f89847d6b6119e9c5e5ea
rcarlosdeb <rcarlosdeb@gmail.com>
2019-11-04 16:24:19 -0600

    cambio en el post de nuevo usuario

--- public_html/apis/singup.php
+++ public_html/apis/singup.php
@@ -7,18 +7,24 @@ if(!$user->nombres){
 }else if(!$user->email){
     sendResponse(400, [] , 'Correo Requiredo!');  
 }else if(!$user->pass){
-    sendResponse(400, [] , 'Contraseña Requiredo!');        
+    sendResponse(400, [] , 'Contrasenia Requiredo!');        
 }else if(!$user->id_tipo_usuario){
-    sendResponse(400, [] , 'Tipo usuario Requiredo!');  
+    sendResponse(400, [] , 'Tipo usuario Requiredo!'); 
+}else if(!$user->apellidos){
+    sendResponse(400, [] , 'Apellidos Requiredo!'); 
+}else if(!$user->direccion){
+    sendResponse(400, [] , 'Direccion Requiredo!');
+}else if(!$user->user){
+    sendResponse(400, [] , 'User Requiredo!');
 }else{
     $password = doEncrypt($user->pass);
     $conn=getConnection();
     if($conn==null){
         sendResponse(500,$conn,'Error de coneccion con el servidor');
     }else{
-        $sql="INSERT INTO usuario(nombre_usuario, email, pass, id_tipo_usuario)";
-        $sql .= "VALUES ('".$user->nombres."','".$user->email."','";
-        $sql .= $password."','".$user->id_tipo_usuario."')";
+        $sql="INSERT INTO usuario(nombre_usuario, apellido_usuario, user, pass, email, direccion, id_tipo_usuario)";
+        $sql .= "VALUES ('".$user->nombres."','".$user->apellidos."','".$user->user."','".$password."','".$user->email."','";
+        $sql .= $user->direccion."','".$user->id_tipo_usuario."')";
         $result = $conn->query($sql);
         if ($result) {
             sendResponse(200, $result , 'Registro realizado correctamente.');
