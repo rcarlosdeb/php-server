0b75de2c11d4e6779c552fb4e729d190e85aeb83
rcarlosdeb <rcarlosdeb@gmail.com>
2019-11-18 22:47:30 +0000

    Agregando codigo a la respuesta

--- public_html/common/response.php
+++ public_html/common/response.php
@@ -3,5 +3,6 @@
  * function for sendning response to uesr
  */
 function sendResponse($resp_code,$data,$message){
+    http_response_code($resp_code);
     echo json_encode(array('code'=>$resp_code,'message'=>$message,'data'=>$data));
 }
