6d3062a65b2d06e15c9d0be0ae354fb1103d4470
rcarlosdeb <rcarlosdeb@gmail.com>
2019-11-23 16:16:03 -0600

    cambio de contraseña

--- mysql/data.sql
+++ mysql/data.sql
@@ -1,7 +1,16 @@
 INSERT INTO tipo_usuario(nombre, descripcion, activo) VALUES("Administrador", "Usuario administrador que puede estadisticas",true);
 INSERT INTO tipo_usuario(nombre, descripcion, activo) VALUES("Asistente", "Usuario que marca asistencia",true);
 INSERT INTO tipo_usuario(nombre, descripcion, activo) VALUES("Docente", "Usuario que imparte materias",true);
-INSERT INTO usuario(nombre_usuario,apellido_usuario,user,pass,email,direccion,id_tipo_usuario) VALUES("Roberto Carlos","Castro","rcarlos","2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394","rcarlos@gmail.com","Santa Ana, Santa Ana",1);
+INSERT INTO usuario(nombre_usuario,apellido_usuario,user,pass,email,direccion,id_tipo_usuario) VALUES("Roberto Carlos","Castro","rcarlos","ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f","rcarlos@gmail.com","Santa Ana, Santa Ana",1);
 INSERT INTO usuario(nombre_usuario,apellido_usuario,user,pass,email,direccion,id_tipo_usuario) VALUES("Nelson
-Humbero","Gochez","ngochez","2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394","ngochez@gmail.com","Santa Ana, Santa Ana",2);
-INSERT INTO usuario(nombre_usuario,apellido_usuario,user,pass,email,direccion,id_tipo_usuario) VALUES("William","Zamora","wzamora","2634c3097f98e36865f0c572009c4ffd73316bc8b88ccfe8d196af35f46e2394","wzamora@gmail.com","Santa Ana, Santa Ana",3);
+Humbero","Gochez","ngochez","ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f","ngochez@gmail.com","Santa Ana, Santa Ana",2);
+INSERT INTO usuario(nombre_usuario,apellido_usuario,user,pass,email,direccion,id_tipo_usuario) VALUES("William","Zamora","wzamora","ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f","wzamora@gmail.com","Santa Ana, Santa Ana",3);
+INSERT INTO departamento(nombre_departamento, codigo_departamento,facultad) VALUES ("Ingenieria", "1234","Facultad de occidente");
+INSERT INTO departamento(nombre_departamento, codigo_departamento,facultad) VALUES ("Matematicas", "1235","Facultad de occidente");
+INSERT INTO departamento(nombre_departamento, codigo_departamento,facultad) VALUES ("Psicologia", "1236","Facultad de occidente");
+INSERT INTO carrera(nombre_carrera,codigo_carrera,activo,id_departamento) VALUES("Ingenieria de sistemas","I30515-1998", true, 1);
+INSERT INTO carrera(nombre_carrera,codigo_carrera,activo,id_departamento) VALUES("Ingenieria industrial","I30513-2015", true, 1);
+INSERT INTO carrera(nombre_carrera,codigo_carrera,activo,id_departamento) VALUES("Ingenieria electrica","I30510-1998", true, 1);
+
+
+
